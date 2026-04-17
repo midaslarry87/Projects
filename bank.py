c579dd5afb309fb9be20bc186dcb33da2c408219
midas larry <midaslarryai@gmail.com>
2026-04-17 00:27:57 -0400

    add the else statement

--- bank.py
+++ bank.py
@@ -1,13 +1,19 @@
 
 Greetings = input("Greeting: ")
 
+
+if Greetings.startswith("hey"):
+    print("$20")
+
+else:
+    print("$100")
+
 match Greetings:
     case "hello" | "Hello":
         print("$0")
 
 
-if Greetings.startswith("h"):
-    print("$20")
 
-    
+
+
 
