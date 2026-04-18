a33af88849ff7799d95a0bd912f725b6b2f5055b
midas larry <midaslarryai@gmail.com>
2026-04-17 20:40:33 -0400

    added a new variable and fix the issue with when  there is more statement after the hello

--- bank.py
+++ bank.py
@@ -1,16 +1,34 @@
 
-Greetings = input("Greeting: ")
+Greetings = input("Greeting: ").lower().strip()
+
+take = Greetings.replace(",","").split()
+
+print(take[0])
+
+# newGreets = Greetings.removeprefix()
+
+# print(newGreets)
+# print(Greetings)
+
+if Greetings == "hello":
+    print("$0")
 
 
-if Greetings.startswith("hey"):
+elif Greetings[0].startswith("h") and Greetings != "hello":
+
     print("$20")
+
 
 else:
     print("$100")
+ 
+# if Greetings.startswith("hey"):
+#     print("$20")
 
-match Greetings:
-    case "hello" | "Hello":
-        print("$0")
+
+# match Greetings:
+#     case "hello" | "Hello":
+#         print("$0")
 
 
 
