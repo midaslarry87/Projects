04cdbcaa2c27e5485133195361cf7c29998ae14e
midas larry <midaslarryai@gmail.com>
2026-04-19 11:38:48 -0400

    initial commit

---
+++ extensions.py
@@ -0,0 +1,22 @@
+
+imageFile = "gif", ".jpg", ".png", ".jpeg"
+applicationFile = ".pdf", "zip"
+textFile = ".text"
+
+fileTypeName = input("File name: ").strip().split(".")
+
+print(fileTypeName[0])
+print(fileTypeName[1])
+
+
+fileTypeName
+
+if fileTypeName[1] in imageFile:
+    print("image/",fileTypeName[1])
+
+elif fileTypeName[1] in applicationFile:
+    print("application/",fileTypeName[1])
+
+print(fileTypeName)
+
+# if 
