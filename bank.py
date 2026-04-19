b9ca40b170267a8b470c574d65fdc004ab0867a3
midas larry <midaslarryai@gmail.com>
2026-04-19 12:19:08 -0400

    fix the comma issue and the else if validation

--- bank.py
+++ bank.py
@@ -1,37 +1,19 @@
-
+# Accept input from the user and make the case insensitive and remove any left, right whitespaces 
 Greetings = input("Greeting: ").lower().strip()
 
+# Remove or omit any comma and replace it with no whitespace and split the input into list.
 take = Greetings.replace(",","").split()
 
-print(take[0])
-
-# newGreets = Greetings.removeprefix()
-
-# print(newGreets)
-# print(Greetings)
-
-if Greetings == "hello":
+# check the input if the word is hello 
+if take[0] == "hello":
     print("$0")
 
-
-elif Greetings[0].startswith("h") and Greetings != "hello":
+# check if the beginning of the word start with h and not hello
+elif take[0].startswith("h") and take[0] != "hello":
 
     print("$20")
 
-
+# print $100 if no word matches hello or start with a word with "h"
 else:
     print("$100")
- 
-# if Greetings.startswith("hey"):
-#     print("$20")
-
-
-# match Greetings:
-#     case "hello" | "Hello":
-#         print("$0")
-
-
-
-
-
 
