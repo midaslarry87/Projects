1fed795932605d086c3213189a09ff0429341e79
midas larry <midaslarryai@gmail.com>
2026-04-27 00:43:01 -0400

    final solution using eval

--- interpreter.py
+++ interpreter.py
@@ -1,79 +1,9 @@
 
-# def myCount (f, g, h):
-    # getCal = input("Expression: ")
-
-    # print(getCal.strip().split())
-
-    # print(getCal.strip().split(","))
-
-    # q = getCal.split(" ")
-
-    # print(q[0])
-    # print(type(q[0]))
-
-    # print(q[1])
-
-    # print(q[2])
-
-    # print(type(q))
-
-    # print("This is below the type of q")
-
-    # f = float(q[0])
-    # g = q[1]
-    # h = float(q[2])
-
-    # return 
-
-# totalCount = (f , g, h )
-
-# print(float(totalCount))
-
-# print(totalCount)
-
-# myCount()
-
-# getCal = input("Expression: ")
-
-# print(getCal.strip().split())
-
-# print(getCal.strip().split(","))
-
-# q = getCal.split(" ")
-
-# print(q[0])
-# print(type(q[0]))
-
-# print(q[1])
-
-# print(q[2])
-
-# print(type(q))
-
-# print("This is below the type of q")
-
-# f = float(q[0])
-# g = q[1].
-# h = float(q[2])
-
-# p = g.strip('')
-# print(type(p))
-
-# print(eval(f,p,h))
-# # if g.re
-# #     print(g)
-# #     print(type(g))
-# #     pri
-
-
 def ConvString():
     getCal = input("Expression: ")
 
     expression = eval(getCal)
 
     print(f"{expression:.1f}")
-
-    # print(expression)
-
 
 ConvString()
