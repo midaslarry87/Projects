bcba48c961aceac1ccb00ba3c7de0308ca9cee06
midas larry <midaslarryai@gmail.com>
2026-04-27 22:04:03 -0400

    using if else statement

--- interpreter.py
+++ interpreter.py
@@ -1,9 +1,44 @@
 
-def ConvString():
-    getCal = input("Expression: ")
+# def ConvString():
+#     getCal = input("Expression: ")
 
-    expression = eval(getCal)
+#     expression = eval(getCal)
 
-    print(f"{expression:.1f}")
+#     print(f"{expression:.1f}")
 
-ConvString()
+# ConvString()
+
+
+# My way of solving this issue 
+
+def getValue ():
+    acceptString = input("Expression: ")
+
+    stringSlice = acceptString.strip().split()
+
+    print(stringSlice)
+
+    f = float(stringSlice[0])
+    g = stringSlice[1]
+    h = float(stringSlice[2])
+
+    print(f, g, h)
+
+    if g == '+':
+        getCal = f + h
+    elif g == '-':
+        getCal = f - h
+
+    elif g == '*':
+        getCal = f * h
+
+    elif g == '/':
+        getCal = f / h 
+
+    # getCal = f"{f + {g} + h}"
+
+    print(type(getCal))
+
+    print(getCal)
+
+getValue()
