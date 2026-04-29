65e3ca771dd264a38f921c4c2873d2cb70c2c9b6
midas larry <midaslarryai@gmail.com>
2026-04-28 23:04:49 -0400

    initial commit

---
+++ meal.py
@@ -0,0 +1,21 @@
+# This is a code for telling user when to eat and what type of food is the person eating during the time of the day 
+
+askTime = input("What time is it? ")
+
+MorningTime = "7:00" 
+AfternoonTime = "12:00" 
+NightTime = "18:00" 
+
+
+if askTime == MorningTime:
+    print("breakfast time")
+
+elif askTime == AfternoonTime: 
+    print("lunch time")
+
+elif askTime == NightTime:
+    print("dinner time")
+
+
+else:
+    print()
