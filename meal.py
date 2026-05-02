30d30b77f0b72d09aac165a648b9c39c0f45ed4f
midas larry <midaslarryai@gmail.com>
2026-05-02 15:30:38 -0400

    re-arranging and organizing the code structure

--- meal.py
+++ meal.py
@@ -1,33 +1,35 @@
 # This is a code for telling user when to eat and what type of food is the person eating during the time of the day 
 
-askTime = input("What time is it? ")
-
 MorningTime = "7:00" 
 AfternoonTime = "12:00" 
 NightTime = "18:00" 
 
 
-if askTime == MorningTime:
-    print("breakfast time")
-
-elif askTime == AfternoonTime: 
-    print("lunch time")
-
-elif askTime == NightTime:
-    print("dinner time")
-
-
-else:
-    print()
 
 def main():
-    pass
 
+    global askTime
+
+    askTime = input("What time is it? ")
+    
 
 def convert(time):
-    pass
 
+
+    
+    if askTime == MorningTime:
+        print("breakfast time")
+
+
+    elif askTime == AfternoonTime: 
+        print("lunch time")
+
+    elif askTime == NightTime:
+        print("dinner time")
+
+    else:
+        print()
 
 if __name__ == "__main__":
-
+    
     main()
