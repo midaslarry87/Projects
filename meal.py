6095dd1b1bf545feaea56077e3dd29c0f78c80d4
midas larry <midaslarryai@gmail.com>
2026-05-04 21:28:24 -0400

    new change with function

--- meal.py
+++ meal.py
@@ -1,20 +1,31 @@
 # This is a code for telling user when to eat and what type of food is the person eating during the time of the day 
 
-MorningTime = "7:00" 
-AfternoonTime = "12:00" 
-NightTime = "18:00" 
+MorningTime = "7:00 - 8:00" 
+AfternoonTime = "12:00 - 13:00" 
+NightTime = "18:00 - 19:00" 
 
+
+# Morning = 
 
 
 def main():
 
     global askTime
+    global spString
 
     askTime = input("What time is it? ")
-    
+
+    spString = askTime.split(":")
+
+    print(spString[0])
+    print(type(spString))
+
+    if spString[0] == "7" or spString[0] == "8":
+        print("breakfast time")    
 
 def convert(time):
 
+    
 
     
     if askTime == MorningTime:
@@ -31,5 +42,5 @@ def convert(time):
         print()
 
 if __name__ == "__main__":
-    
+
     main()
