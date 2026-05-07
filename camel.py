c00d440508ee1130cb72406b81394aa06c3542a6
midas larry <midaslarryai@gmail.com>
2026-05-06 22:17:48 -0400

    added the first for loop to check for uppercase letter

--- camel.py
+++ camel.py
@@ -3,6 +3,12 @@ getName = input("camelCase: ")
 
 
 if getName.title():
+    for n in getName:
+        if n in n.upper():
+            print(n)
+            # print(n.lower())
+    
+    getName.split()
     print("snake_case:", getName)
 
 
