835d46e6a78e74096fada74a69a498aae98f2046
midas larry <midaslarryai@gmail.com>
2026-05-18 01:40:42 -0400

    initial commit

---
+++ plates.py
@@ -0,0 +1,18 @@
+
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("valid")
+
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    pass
+
+
+main()
+
+
+    
