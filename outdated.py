8e8a6927a38238011a2faab006c0d35f7e101b13
midas larry <midaslarryai@gmail.com>
2026-05-30 17:14:18 -0400

    initial commit

---
+++ outdated.py
@@ -0,0 +1,26 @@
+
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+dates = input("Date: ").split("/")
+
+print(dates)
+
+newDate = dates
+
+
+print("-0".join(reversed(newDate)))
+
+
